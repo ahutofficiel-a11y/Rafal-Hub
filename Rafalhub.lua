b1f8511dd4d6c92546df8f2a448f445caa77692c
ahutofficiel-a11y <cacahuete.officiel@gmail.com>
2025-12-29 22:28:57 +0100

    Rafalhub.lua

--- Rafalhub.lua
+++ Rafalhub.lua
@@ -1,438 +1,173 @@
-local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
 
-local Window = Rayfield:CreateWindow({
-   Name = "🎄RAFAL HUB V1.3🎄",
-   Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
-   LoadingTitle = "RAFAL HUB",
-   LoadingSubtitle = "key in discord",
-   ShowText = "Rayfield", -- for mobile users to unhide rayfield, change if you'd like
-   Theme = "Default", -- Check https://docs.sirius.menu/rayfield/configuration/themes
-
-   ToggleUIKeybind = "K", -- The keybind to toggle the UI visibility (string like "K" or Enum.KeyCode)
-
-   DisableRayfieldPrompts = false,
-   DisableBuildWarnings = false, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
-
-   ConfigurationSaving = {
-      Enabled = false,
-      FolderName = nil, -- Create a custom folder for your hub/game
-      FileName = "Big Hub"
-   },
-
-   Discord = {
-      Enabled = true, -- Prompt the user to join your Discord server if their executor supports it
-      Invite = "m7Gzcy9tJt", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ ABCD would be ABCD
-      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
-   },
-
-   KeySystem = true, -- Set this to true to use our key system
-   KeySettings = {
-      Title = "🎄Alpha Hub🎄",
-      Subtitle = "Key System",
-      Note = "Join https://discord.gg/m7Gzcy9tJt", -- Use this to tell the user how to get a key
-      FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
-      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
-      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-      Key = {"AlphaKey2"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
-   }
+WindUI:AddTheme({
+    Name = "Cacahuète theme", -- theme name
+    
+    Accent = Color3.fromHex("#18181b"),
+    Background = Color3.fromHex("#101010"), -- Accent
+    Outline = Color3.fromHex("#FFFFFF"),
+    Text = Color3.fromHex("#FFFFFF"),
+    Placeholder = Color3.fromHex("#7a7a7a"),
+    Button = Color3.fromHex("#52525b"),
+    Icon = Color3.fromHex("#a1a1aa"),
 })
 
-local MainTab = Window:CreateTab("Informations", nil) -- Title, Image
-local MainSection = MainTab:CreateSection("Main")
-
-Rayfield:Notify({
-   Title = "Script Executed",
-   Content = "Thanks you using rafal hub",
-   Duration = 5,
-   Image = nil,
+local Window = WindUI:CreateWindow({
+    Title = "Alpha Hub V.2",
+    Icon = "door-open", -- lucide icon
+    Author = "by Cacahuète and Thomas",
+    Folder = "AplhaHubV2",
+    
+    -- ↓ This all is Optional. You can remove it.
+    Size = UDim2.fromOffset(580, 460),
+    MinSize = Vector2.new(560, 350),
+    MaxSize = Vector2.new(850, 560),
+    Transparent = true,
+    Theme = "Dark",
+    Resizable = true,
+    SideBarWidth = 200,
+    BackgroundImageTransparency = 0.42,
+    HideSearchBar = true,
+    ScrollBarEnabled = false,
+    
+    -- ↓ Optional. You can remove it.
+    --[[ You can set 'rbxassetid://' or video to Background.
+        'rbxassetid://':
+            Background = "rbxassetid://", -- rbxassetid
+        Video:
+            Background = "video:YOUR-RAW-LINK-TO-VIDEO.webm", -- video 
+    --]]
+    
+    -- ↓ Optional. You can remove it.
+    User = {
+        Enabled = true,
+        Anonymous = true,
+        Callback = function()
+            print("clicked")
+        end,
+    },
+    
+    --       remove this all, 
+    -- !  ↓  if you DON'T need the key system
+    KeySystem = { 
+        -- ↓ Optional. You can remove it.
+        Key = { "AlphaHubKey1" },
+        
+        Note = "key system",
+        
+        -- ↓ Optional. You can remove it.
+        Thumbnail = {
+            Image = "rbxassetid://",
+            Title = "Thumbnail",
+        },
+        
+        -- ↓ Optional. You can remove it.
+        URL = "https://discord.gg/xcuerzrYXj",
+        
+        -- ↓ Optional. You can remove it.
+        SaveKey = true, -- automatically save and load the key.
+        
+        -- ↓ Optional. You can remove it.
+        -- API = {} ← Services. Read about it below ↓
+    },
 })
 
-local Label = MainTab:CreateLabel("Discord : https://discord.gg/m7Gzcy9tJt", 4483362458, Color3.fromRGB(255, 255, 255), false) -- Title, Icon, Color, IgnoreTheme
-
-local Paragraph = MainTab:CreateParagraph({Title = "Credits", Content = "This script was created by Anonyme and Anonyme"})
-
-local MainTab = Window:CreateTab("Normal", nil) -- Title, Image
-local MainSection = MainTab:CreateSection("All scripts")
-
-local Button = MainTab:CreateButton({
-   Name = "Infinite Jump",
-   Callback = function()
-       loadstring(game:HttpGet("https://obj.wearedevs.net/2/scripts/Infinite%20Jump.lua"))()
-   end,
+local Tab = Window:Tab({
+    Title = "Basic",
+    Icon = "bird", -- optional
+    Locked = false,
 })
 
-local Button = MainTab:CreateButton({
-   Name = "Private Server [FREE]",
-   Callback = function()
-       loadstring(game:HttpGet("https://raw.githubusercontent.com/tienkhanh1/spicy/main/PrivateServer"))()
-   end,
+local Section = Window:Section({
+    Title = "Informations",
+    Icon = "bird",
+    Opened = true,
 })
 
-local Button = MainTab:CreateButton({
-   Name = "ESP",
-   Callback = function()
-       loadstring(game:HttpGet("https://raw.githubusercontent.com/ahutofficiel-a11y/Rafal-Hub/refs/heads/main/Esp.lua"))()
-   end,
+local Dialog = Window:Dialog({
+    Icon = "bird",
+    Title = "All cheat basic",
+    Content = "Hi, my cheat is open for everyone, good cheat for everyone !!!",
+    Buttons = {
+        {
+            Title = "Confirm",
+            Callback = function()
+                print("Confirmed!")
+            end,
+        },
+        {
+            Title = "Cancel",
+            Callback = function()
+                print("Cancelled!")
+            end,
+        },
+    },
 })
 
-local Button = MainTab:CreateButton({
-   Name = "Aimlock [BÊTA]",
-   Callback = function()
-       loadstring(game:HttpGet("https://raw.githubusercontent.com/ahutofficiel-a11y/Rafal-Hub/refs/heads/main/aimlock.lua"))()
-   end,
+Window:Tag({
+    Title = "v2.0.0",
+    Icon = "github",
+    Color = Color3.fromHex("#30ff6a"),
+    Radius = 0, -- from 0 to 13
 })
 
-local Button = MainTab:CreateButton({
-   Name = "God Mod [BÊTA]",
-   Callback = function()
-       -- Script à placer dans ServerScriptService
-
-game.Players.PlayerAdded:Connect(function(player)
-    player.CharacterAdded:Connect(function(char)
-        local humanoid = char:WaitForChild("Humanoid")
-
-        -- Santé infinie
-        humanoid.MaxHealth = math.huge
-        humanoid.Health = math.huge
-
-        -- Si tu veux qu’il se soigne en cas de dégâts :
-        humanoid.HealthChanged:Connect(function()
-            if humanoid.Health < humanoid.MaxHealth then
-                humanoid.Health = humanoid.MaxHealth
-            end
-        end)
-    end)
-end)
-
-   end,
+WindUI:Notify({
+    Title = "Welcome",
+    Content = "Thanks for using Alpha Hub",
+    Duration = 3, -- 3 seconds
+    Icon = "bird",
 })
 
-local Button = MainTab:CreateButton({
-   Name = "Give Tools",
-   Callback = function()
-       loadstring(game:HttpGet("https://pastefy.app/qpbQo0lr/raw"))()
-   end,
-})
-
-local Button = MainTab:CreateButton({
-   Name = "Véhicule Mods [NEW]",
-   Callback = function()
-       loadstring(game:HttpGet('https://raw.githubusercontent.com/Documantation12/Universal-Vehicle-Script/main/Main.lua'))()
-   end,
-})
-
-local Button = MainTab:CreateButton({
-   Name = "Fly",
-   Callback = function()
-       loadstring(game:HttpGet("https://raw.githubusercontent.com/XNEOFF/FlyGuiV3/main/FlyGuiV3.txt"))()
-   end,
-})
-
-local Button = MainTab:CreateButton({
-   Name = "Anti-AFK",
-   Callback = function()
-       loadstring(game:HttpGet("https://obj.wearedevs.net/2/scripts/Focus%20Anti-AFK.lua"))()
-   end,
-})
-
-local Button = MainTab:CreateButton({
-   Name = "Infinity Yeld",
-   Callback = function()
-       loadstring(game:HttpGet("https://obj.wearedevs.net/2/scripts/Infinite%20Yield.lua"))()
-   end,
-})
-
-local Slider = MainTab:CreateSlider({
-   Name = "WalkSpeed slider",
-   Range = {16, 500},
-   Increment = 1,
-   Suffix = "Speed",
-   CurrentValue = 16,
-   Flag = "Slider1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
-   Callback = function(Value)
-        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = (Value)
-   end,
-})
-
-local Slider = MainTab:CreateSlider({
-   Name = "JumpPower slider",
-   Range = {50, 500},
-   Increment = 1,
-   Suffix = "Jump",
-   CurrentValue = 50,
-   Flag = "Slider2", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
-   Callback = function(Value)
-        game.Players.LocalPlayer.Character.Humanoid.JumpPower = (Value)
-   end,
-})
-
-local Slider = MainTab:CreateSlider({
-   Name = "JumpHeight slider",
-   Range = {7.5, 500},
-   Increment = 7.5,
-   Suffix = "Jump",
-   CurrentValue = 50,
-   Flag = "Slider3", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
-   Callback = function(Value)
-        game.Players.LocalPlayer.Character.Humanoid.JumpHeight = (Value)
-   end,
-})
-
-local MainTab = Window:CreateTab("Brookhaven", nil) -- Title, Image
-local MainSection = MainTab:CreateSection("Brookhaven Teleport")
-
-local Button = MainTab:CreateButton({
-   Name = "Spawn",
-   Callback = function()
-       local Players = game:GetService("Players")
-local player = Players.LocalPlayer
-
-local targetPosition = Vector3.new(0, 10, 0) -- X, Y, Z
-
-local function teleportToCoords(position)
-    if player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
-        player.Character.HumanoidRootPart.CFrame = CFrame.new(position)
+local Button = Tab:Button({
+    Title = "Infinite Jump",
+    Desc = "Infinite Jump !!!!!!!!!!!!",
+    Locked = false,
+    Callback = function()
+         loadstring(game:HttpGet("https://obj.wearedevs.net/2/scripts/Infinite%20Jump.lua"))()
     end
-end
-
-task.wait(0.1)
-teleportToCoords(targetPosition)
-   end,
 })
 
-local Button = MainTab:CreateButton({
-   Name = "Bank",
-   Callback = function()
-       local Players = game:GetService("Players")
-local player = Players.LocalPlayer
-
-local targetPosition = Vector3.new(-5,17,254) -- X, Y, Z
-
-local function teleportToCoords(position)
-    if player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
-        player.Character.HumanoidRootPart.CFrame = CFrame.new(position)
+local Button = Tab:Button({
+    Title = "Fly",
+    Desc = "Fly for all games",
+    Locked = false,
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/XNEOFF/FlyGuiV3/main/FlyGuiV3.txt"))()
     end
-end
-
-task.wait(0.1)
-teleportToCoords(targetPosition)
-   end,
 })
 
-local Button = MainTab:CreateButton({
-   Name = "Hopital",
-   Callback = function()
-       local Players = game:GetService("Players")
-local player = Players.LocalPlayer
-
-local targetPosition = Vector3.new(-305,3,27) -- X, Y, Z
-
-local function teleportToCoords(position)
-    if player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
-        player.Character.HumanoidRootPart.CFrame = CFrame.new(position)
+local Button = Tab:Button({
+    Title = "Anti-Afk",
+    Desc = "Anti-Afk for all games",
+    Locked = false,
+    Callback = function()
+        loadstring(game:HttpGet("https://obj.wearedevs.net/2/scripts/Focus%20Anti-AFK.lua"))()
     end
-end
-
-task.wait(0.1)
-teleportToCoords(targetPosition)
-   end,
 })
 
-local Button = MainTab:CreateButton({
-   Name = "Airport",
-   Callback = function()
-       local Players = game:GetService("Players")
-local player = Players.LocalPlayer
-
-local targetPosition = Vector3.new(318,4,44) -- X, Y, Z
-
-local function teleportToCoords(position)
-    if player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
-        player.Character.HumanoidRootPart.CFrame = CFrame.new(position)
+local Button = Tab:Button({
+    Title = "Aimlock",
+    Desc = "Aimlock for all games",
+    Locked = false,
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/ahutofficiel-a11y/Rafal-Hub/refs/heads/main/aimlock.lua"))()
     end
-end
-
-task.wait(0.1)
-teleportToCoords(targetPosition)
-   end,
 })
 
-local MainTab = Window:CreateTab("Mega Obby", nil) -- Title, Image
-local MainSection = MainTab:CreateSection("Maga Obby Teleport")
-
-local Button = MainTab:CreateButton({
-   Name = "End",
-   Callback = function()
-       local Players = game:GetService("Players")
-local player = Players.LocalPlayer
-
-local targetPosition = Vector3.new(-1089,536,423) -- X, Y, Z
-
-local function teleportToCoords(position)
-    if player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
-        player.Character.HumanoidRootPart.CFrame = CFrame.new(position)
+local Button = Tab:Button({
+    Title = "ESP",
+    Desc = "ESP for all games",
+    Locked = false,
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/ahutofficiel-a11y/Rafal-Hub/refs/heads/main/Esp.lua"))()
     end
-end
-
-task.wait(0.1)
-teleportToCoords(targetPosition)
-   end,
 })
 
-local MainTab = Window:CreateTab("Fun Squid Game! Glass Bridge 2", nil) -- Title, Image
-local MainSection = MainTab:CreateSection("Fun Squid Game! Glass Bridge 2 teleport")
-
-local Button = MainTab:CreateButton({
-   Name = "End",
-   Callback = function()
-       local Players = game:GetService("Players")
-local player = Players.LocalPlayer
-
-local targetPosition = Vector3.new(-114,14,-1329) -- X, Y, Z
-
-local function teleportToCoords(position)
-    if player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
-        player.Character.HumanoidRootPart.CFrame = CFrame.new(position)
+local Button = Tab:Button({
+    Title = "Private Server",
+    Desc = "Private Server for everyone",
+    Locked = false,
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/tienkhanh1/spicy/main/PrivateServer"))
     end
-end
-
-task.wait(0.1)
-teleportToCoords(targetPosition)
-   end,
-})
-
-local MainTab = Window:CreateTab("Emergency Hamburg", nil) -- Title, Image
-local MainSection = MainTab:CreateSection("Script for emergency hamburg")
-
-local Button = MainTab:CreateButton({
-   Name = "Lunar menu",
-   Callback = function()
-       loadstring(Game:HttpGet("https://raw.githubusercontent.com/fluxscripts/LunarV1/refs/heads/main/mainscript"))();
-   end,
-})
-
-local MainTab = Window:CreateTab("Secours de France", nil) -- Title, Image
-local MainSection = MainTab:CreateSection("Script for Secours de France")
-
-local Button = MainTab:CreateButton({
-   Name = "Secours de France Véhicule Mods",
-   Callback = function()
-       loadstring(game:HttpGet('https://raw.githubusercontent.com/Documantation12/Universal-Vehicle-Script/main/Main.lua'))()
-   end,
-})
-
-local Button = MainTab:CreateButton({
-   Name = "Secours de France BYPASS",
-   Callback = function()
-       local LocalPlayerScripts = game:GetService("Players").LocalPlayer.PlayerScripts
-local IteratorFunc, TableState, IteratorKey = pairs({
-    "GunEngine",
-    "ChatTags",
-    "ClientServerPortal",
-    "Console_BubbleChat",
-    "Lighting_Manager",
-    "PP_Manager",
-    "PlayerScriptsLoader",
-    "RbxCharacterSounds",
-    "Sound_Manager",
-    "Time_Manager",
-    "UI_Manager",
-    "UserInput_Manager",
-    "Weather_Manager",
-    "Zones_Manager",
-    "PlayerModule"
-})
-while true do
-    local ModuleName
-    IteratorKey, ModuleName = IteratorFunc(TableState, IteratorKey)
-    if IteratorKey == nil then
-        break
-    end
-    local ModuleInstance = LocalPlayerScripts:FindFirstChild(ModuleName)
-    if ModuleInstance then
-        ModuleInstance:Destroy()
-    end
-end
-print("Secours de France Bypassed (cheh recovery et vicojump1")
-   end,
-})
-
-local Button = MainTab:CreateButton({
-   Name = "Secours de France MENU",
-   Callback = function()
-       loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/97f604ac3cc5485a240bc78c2c2108f62958e62a62ea9a7e2d6b0fc7a67693c1/download"))()
-   end,
-})
-
-local MainTab = Window:CreateTab("Animations", nil)
-local MainSection = MainTab:CreateSection("Animation for all games")
-
-local Button = MainTab:CreateButton({
-   Name = "JERK OFF",
-   Callback = function()
-       loadstring(game:HttpGet("https://raw.githubusercontent.com/imalwaysad/universal-gui/refs/heads/main/jerk%20off%20r6"))()
-   end,
-})
-
-local MainTab = Window:CreateTab("ACS Killer", nil) -- Title, Image
-local MainSection = MainTab:CreateSection("ACS 1.7.5 and ACS 2.0.1")
-
-local Button = MainTab:CreateButton({
-   Name = "ACS Killer",
-   Callback = function()
-       loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/6203d711b7531ea5ce99f5877762ec82.lua"))()
-   end,
-})
-
-local MainTab = Window:CreateTab("Bypass", nil) -- Title, Image
-local MainSection = MainTab:CreateSection("Stop Anti-Cheat")
-
-local Button = MainTab:CreateButton({
-   Name = "STOP ADONIS ANTI-CHEAT",
-   Callback = function()
-       loadstring(game:HttpGet("https://raw.githubusercontent.com/e1998ee/adonisb1p3ss/refs/heads/main/NeptuneScripts"))()
-   end,
-})
-
-local Button = MainTab:CreateButton({
-   Name = "Bypass Secours de France",
-   Callback = function()
-       local LocalPlayerScripts = game:GetService("Players").LocalPlayer.PlayerScripts
-local IteratorFunc, TableState, IteratorKey = pairs({
-    "GunEngine",
-    "ChatTags",
-    "ClientServerPortal",
-    "Console_BubbleChat",
-    "Lighting_Manager",
-    "PP_Manager",
-    "PlayerScriptsLoader",
-    "RbxCharacterSounds",
-    "Sound_Manager",
-    "Time_Manager",
-    "UI_Manager",
-    "UserInput_Manager",
-    "Weather_Manager",
-    "Zones_Manager",
-    "PlayerModule"
-})
-while true do
-    local ModuleName
-    IteratorKey, ModuleName = IteratorFunc(TableState, IteratorKey)
-    if IteratorKey == nil then
-        break
-    end
-    local ModuleInstance = LocalPlayerScripts:FindFirstChild(ModuleName)
-    if ModuleInstance then
-        ModuleInstance:Destroy()
-    end
-end
-print("Ez Hook Function 0x1324322114235064 / AntiCheat Bypass")
-   end,
-})
-
-local Button = MainTab:CreateButton({
-   Name = "Bypass Voice Chat",
-   Callback = function()
-       game:GetService("VoiceChatService"):joinVoice()
-   end,
 })
